627e1c30f1672b6c422911619aa24de1a6ecc2cf
tzhf <theo.zahaf@gmail.com>
2021-11-06 21:28:04 +0100

    clipboard button changes

--- src/components/CopyToClipboard.vue
+++ src/components/CopyToClipboard.vue
@@ -1,14 +1,17 @@
 <template>
-	<Button @click="copyToClipboard()" text="Clipboard" />
+	<Button @click="copyToClipboard()" :text="text" />
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Button from "./Elements/Button.vue";
 
 const props = defineProps({
 	customMap: Object,
 	data: Array,
 });
+
+const text = ref("Clipboard");
 
 const copyToClipboard = () => {
 	navigator.clipboard
@@ -19,6 +22,12 @@ const copyToClipboard = () => {
 				customCoordinates: props.data,
 			})
 		)
+		.then(() => {
+			text.value = "Copied";
+			setTimeout(() => {
+				text.value = "Clipboard";
+			}, 1000);
+		})
 		.catch((err) => {
 			console.log("Something went wrong", err);
 		});
