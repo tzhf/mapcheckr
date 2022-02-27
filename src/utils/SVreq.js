0e46e7252c90c1244f6908cc8962a714c57afab0
itisem <emily@emily.bz>
2022-02-27 16:26:42 +0100

    small bugfix i just noticed

--- src/utils/SVreq.js
+++ src/utils/SVreq.js
@@ -5,7 +5,7 @@ export default function SVreq(loc, settings) {
 		let callback = (res, status) => {
 			if (status != google.maps.StreetViewStatus.OK) return reject({ ...loc, reason: "sv not found" });
 			if (settings.rejectUnofficial) {
-				if (res.location.pano.length != 22) return reject({ ...loc, reason: res.copyright.substring(6) });
+				if (res.location.pano.length != 22) return reject({ ...loc, reason: "unofficial coverage"  });
 				if (settings.rejectNoDescription && !res.location.description && !res.location.shortDescription) return reject({ ...loc, reason: "no description" });
 			}
 			if (Date.parse(res.imageDate) < Date.parse(settings.fromDate) || Date.parse(res.imageDate) > Date.parse(settings.toDate))
